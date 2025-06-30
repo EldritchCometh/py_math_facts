39bc0b58cdfab1ba7d4ac2e538f95a9bf7aad0cb
EldritchCometh <ABanzhof@gmail.com>
2025-06-29 23:04:57 -0400

    Add files via upload

---
+++ math_facts.py
@@ -0,0 +1,133 @@
+
+import os
+import math
+import pickle
+import random
+from typing import List
+from src.facts_maker import FactsMaker, MathFactDC
+
+
+class MathFacts:
+
+
+    def __init__(self, settings):
+
+        self._settings = settings
+        self._user = settings['user']
+        self._num_facts = settings['num_probs']
+
+        filter = lambda c: c.isalpha() or c.isdigit() or c == '_'
+        self._user = ''.join([c for c in self._user if filter(c)]).lower()
+
+        parent_directory = os.path.dirname(os.path.abspath(__file__))
+        self._data_path = os.path.join(parent_directory, '../app_data')
+        self._user_path = os.path.join(self._data_path, f'{self._user}.pkl')
+
+        self._all_facts: List[MathFactDC] = self._load_data()
+        self._session_facts: List[MathFactDC] = self._get_session_facts()
+        self._current_fact: MathFactDC; self.set_next()
+
+        self._prog_updated_flag = False
+
+
+    @property
+    def equation(self) -> List[str]:
+
+        return self._current_fact.equation
+
+
+    @property
+    def solution(self) -> int:
+        
+        return self._current_fact.solution
+    
+
+    @property
+    def mastry_progress(self) -> int:
+
+        return self._current_fact.progress
+        
+
+    @property
+    def percent_completed(self) -> int:
+        
+        num_completed = self._num_facts - len(self._session_facts) - 1
+        return num_completed / self._num_facts
+
+
+    @property
+    def percent_mastered(self) -> float:
+
+        mastered = sum(1 for p in self._all_facts if p.mastered)
+        return mastered / len(self._all_facts)
+
+
+    @property
+    def timer_duration(self) -> int:
+        
+        try:
+            times = [0] + self._settings['times']
+            return times[self.mastry_progress]
+        except:
+            return 0
+        
+
+    @property
+    def facts_remaining(self) -> int:
+        
+        return len(self._session_facts)
+
+
+    def set_next(self) -> None:
+        
+        self._current_fact = random.choice(self._session_facts)
+        self._session_facts.remove(self._current_fact)
+        self._prog_updated_flag = False
+
+
+    def update_progress(self, answered_correctly: bool) -> None:
+
+        if self._prog_updated_flag:
+            return
+
+        prog_threshold = 1 + len(self._settings['times'])
+
+        progress = self._current_fact.progress
+        progress += answered_correctly
+        progress -= (not answered_correctly)
+        progress = max(0, min(prog_threshold, progress))
+        
+        self._current_fact.progress = progress
+        self._current_fact.mastered = (progress == prog_threshold)
+        
+        self._prog_updated_flag = True
+        self._save_data()
+
+
+    def _get_session_facts(self) -> List[MathFactDC]:
+
+        mastered = [p for p in self._all_facts if p.mastered]
+        n = min(len(mastered), math.floor(self._num_facts * 1/3))
+        session_facts = random.sample(mastered, n)
+
+        unmastered = [p for p in self._all_facts if not p.mastered]
+        unmastered.sort(key=lambda p: p.difficulty)
+        session_facts.extend(unmastered[:self._num_facts-len(session_facts)])
+
+        return session_facts
+
+
+    def _save_data(self) -> None:
+
+        os.makedirs(self._data_path, exist_ok=True)
+        with open(self._user_path, 'wb') as f:
+            pickle.dump(self._all_facts, f)
+
+
+    def _load_data(self) -> List[MathFactDC]:
+        
+        if not os.path.exists(self._user_path):
+            return FactsMaker().math_facts
+        
+        with open(self._user_path, 'rb') as file:
+            return pickle.load(file)
---
+++ play_screen.py
@@ -0,0 +1,127 @@
+
+import time
+import tkinter as tk
+from tkinter import ttk
+from typing import Dict, List, Tuple
+import tkinter.font as tkFont
+from typing import Callable, Optional
+import random
+
+
+class PlayScreen(tk.Frame):
+    
+
+    def __init__(self, ui):
+
+        super().__init__()
+        
+        self.app = ui.app
+        self.ps = ui.app.ps
+
+        self._ui_frame = tk.Frame(ui)
+        self._frames: Dict[str, tk.Frame] = {}
+        self._eq_sub_frames: List[tk.Frame] = []
+        self._widgets: Dict[str, tk.Widget] = {}
+        self._font = tkFont.Font(family="Arial")
+        self._after_ids = {}
+
+        self._make_layout()
+
+
+    def populate(self):
+
+        if self._widgets:
+            for widget in self._widgets.values():
+                widget.destroy()
+            self._widgets = {}
+        
+        for i, (t, f) in enumerate(zip(self.ps.equation, self._eq_sub_frames)):
+            if t == '_':
+                widget = tk.Entry(f, width=3, justify='center', font=self._font)
+                self._widgets['entry'] = widget
+                widget.focus_set()
+            else:
+                widget = tk.Label(f, text=t, font=self._font)
+                self._widgets[f'labels_{i}'] = widget
+        
+        prog_bar_names = ['timer', 'progress', 'mastery']
+        values = [0, self.ps.percent_completed, self.ps.percent_mastered]
+        for name, v in zip(prog_bar_names, values):
+            bar = ttk.Progressbar(self._frames[f'{name}_frame'])
+            bar.configure(orient='horizontal', mode='determinate')
+            self._widgets[f'{name}_bar'] = bar
+            bar['maximum'], bar['value'] = 1000, v * 1000
+        
+        self._start_timer()
+        self._widgets['entry'].bind('<Return>', self.app._on_return)
+        self._widgets['entry'].bind('<KP_Enter>', self.app._on_return)
+
+        for widget in self._widgets.values():
+            widget.pack(expand=True, fill='both')
+
+
+    def resize(self, win_width: int, win_height: int):
+        
+        if not self._widgets:
+            return
+        
+        eq_frame_height = win_height * (1-3/8)
+        width_factor = int(win_width * 0.072)
+        height_factor = int(eq_frame_height * 0.45)
+        font_size = min(width_factor, height_factor)
+        self._font.configure(size=font_size)
+        
+
+    def stop_timer(self):
+        if 'update_timer' in self._after_ids:
+            after_id = self._after_ids.pop('update_timer')
+            if after_id is not None:
+                self._ui_frame.after_cancel(after_id)
+
+
+    def _start_timer(self):
+        
+        self.stop_timer()
+        timer_bar = self._widgets['timer_bar']
+        timer_bar['maximum'] = self.ps.timer_duration
+        timer_bar['value'] = 0
+        self._start_time = time.time()
+
+        if self.ps.timer_duration == 0:
+            return
+
+        def update_timer():
+            elapsed = time.time() - self._start_time
+            timer_bar['value'] = elapsed
+            if elapsed < self.ps.timer_duration:
+                self._after_ids['update_timer'] = \
+                    self._ui_frame.after(10, update_timer)
+            else:
+                timer_bar['value'] = self.ps.timer_duration
+                self.app._on_timeup()
+
+        update_timer()
+
+
+    def _make_layout(self):
+
+        frame_names = ['equation_frame', 'timer_frame', 
+                       'progress_frame', 'mastery_frame']
+        row_weights = [8, 1, 1, 1]
+        frame_padys = [(4, 2), (2, 2), (2, 2), (2, 2)]
+        arg_lists = zip(frame_names, row_weights, frame_padys)
+        self._ui_frame.grid_columnconfigure(0, weight=1)
+        for i, (name, weight, pady) in enumerate(arg_lists):
+            frame = tk.Frame(self._ui_frame, borderwidth=5, relief='raised')
+            self._frames[name] = frame
+            self._ui_frame.grid_rowconfigure(i, weight=weight)    
+            frame.grid(row=i, column=0, sticky="nsew", padx=4, pady=pady)
+            frame.pack_propagate(False)
+            
+        eq_inner_frame = tk.Frame(self._frames['equation_frame'])
+        self._eq_sub_frames = [tk.Frame(eq_inner_frame) for _ in range(5)]
+        eq_inner_frame.grid_rowconfigure(0, weight=1)
+        for i, sf in enumerate(self._eq_sub_frames):
+            eq_inner_frame.grid_columnconfigure(i, weight=1)
+            sf.grid(row=0, column=i, sticky="nsew", padx=2, pady=2)
+        eq_inner_frame.pack(expand=True, fill='both')
